b73c59c5548000429b16478fbb5515994acc5c90
nakisa <nakisa.donnelly@gmail.com>
2021-05-13 15:52:47 -0700

    Added Unity Project

---
+++ Apr2021-XRDEV-AN/Assets/Scripts/PlanetRotation.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetRotation : MonoBehaviour
+{
+    public Vector3 axisOfRotation; // (x, y, z)
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("start has been called");
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(axisOfRotation * Time.deltaTime * speed);
+
+    }
+}
